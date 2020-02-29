454d5ceefd2da0bb6b6fe8a6ed796dd114f8d332
Dat Nguyen <DN@Dats-MacBook-Air.local>
2020-02-29 16:04:53 +0700

    fix render child nodes

---
+++ src/Helpers/Common.js
@@ -0,0 +1,14 @@
+
+export const cRandomNumber = () => {
+    return Math.floor((Math.random() * 5) + 1);
+};
+
+export const cMakeID = (length) => {
+    let result           = '';
+    let characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let charactersLength = characters.length;
+    for ( let i = 0; i < length; i++ ) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
---
+++ src/Home.js
@@ -0,0 +1,219 @@
+import React from 'react';
+import ReactDOM from "react-dom";
+import './Home.css';
+import $ from 'jquery';
+import 'jquery-ui';
+import 'jquery-ui-dist/jquery-ui';
+import * as HelperCommon from './Helpers/Common.js';
+
+const width = 48;
+
+export default class Home extends React.Component {  
+
+    /**
+     * collapse button event, append rendering child nodes 
+     */
+    collapseChild(number, element) {
+        if (!$(element.target).hasClass('opened')) {
+            let idList = [];
+            let nodeList = [];
+            for (let i = 0; i < number; i++) {
+                let j = (i - 2) >= 0? i : 0;
+                let height = 24 + (87 * i) + (10 * j);
+                let line = [{x:0, y:12}, {x:96, y:12}];
+                let path = '';
+                let c = [{x:0, y:12}, {x:width, y:12}, {x:0, y:(height-1)}, {x:width, y:(height-1)}];
+                if (i === 0)
+                    path = 'M' + line[0].x + ',' + line[0].y + ' ' + line[1].x + ',' + line[1].y;
+                else
+                    path = 'M' + c[0].x + ',' + c[0].y + ' C' + c[1].x + ',' + c[1].y + ' ' + c[2].x + ',' + c[2].y + ' ' + c[3].x + ',' + c[3].y;
+
+                let id = HelperCommon.cMakeID(4);
+                let newNum = HelperCommon.cRandomNumber();
+                let node = (  
+                    <div className="child-wrapper">
+                        <div className="child-item">
+                            <svg className={"item-line-wrapper " + ((i===0)?'item-line-strange':'')} height={height} width="96">
+                                <path d={path}></path>
+                            </svg>
+                            <div className="item-wrapper">
+                                <div className="item-node" id={"draggable_" + id} draggable="true">
+                                    <div className="item-content-container">
+                                        <div className="item-content">
+                                            This is test {i+1}
+                                            <br />
+                                            Total: {newNum}
+                                        </div>
+                                    </div>
+                                    <button className="btn-group-collapse" type="button" onClick={this.collapseChild.bind(this, newNum)}>{newNum} &gt;</button>
+                                </div>
+                                <div className="children-wrapper"></div>
+                            </div>
+                        </div>
+                    </div>
+                );
+                nodeList.push(node);
+                idList.push(id);
+            }
+
+            // render child nodes to .children-wrapper div
+            const appendEl = $(element.target).parent().next();
+            const container = ReactDOM.findDOMNode(appendEl[0]);
+            ReactDOM.render(nodeList, container);
+
+            // update parent, draw lines
+            this._updateParent(element.target);
+            this._initDropNode();
+
+            // update button status class
+            $(element.target).addClass('opened');
+        } else {
+            if ($(element.target).parent().next().is('div')) {
+                $(element.target).removeClass('opened');
+                // destroy all child nodes
+                ReactDOM.unmountComponentAtNode($(element.target).parent().next()[0]);
+                this._updateParent(element.target);
+            }
+        }
+    }
+
+    /**
+     * recusive updating parents of current clicked button
+     */
+    _updateParent(element) {
+        let parent = $(element).parent().parent().parent().parent();
+        if ($(parent).attr('class') === 'child-wrapper') {
+            this._updateLine($(parent));
+            this._updateParent($(parent));
+        }
+    }
+    
+    /**
+     * calculating again svg and path for all next elements
+     */
+    _updateLine(element) {
+        if ($(element).next().is('div')){
+            let height = 0;
+            let childList = $(element).parent().children();
+            $.each(childList, function( index, value ) {
+                if (index <= $(element).index()) {
+                    height += $(value).height() + 24;
+                }
+            });
+            $(element).next().find('svg').first().attr('height', height);
+            let c_update = [{x:0, y:12}, {x:width, y:12}, {x:0, y:(height-1)}, {x:width, y:(height-1)}];
+            let path_update = 'M' + c_update[0].x + ',' + c_update[0].y + ' C' + c_update[1].x + ',' + c_update[1].y + ' ' + c_update[2].x + ',' + c_update[2].y + ' ' + c_update[3].x + ',' + c_update[3].y;
+            $(element).next().find('svg').find('path').first().attr('d', path_update);
+            this._updateLine($(element).next());
+        }
+    
+    }
+
+    /**
+     * init drag drop event for item-node class element
+     */
+    _initDropNode() {
+        var self = this;
+        $('.item-node').each(function(index, value) {
+            let element_id = $(value).attr('id');
+            let element = '#' + element_id;
+            $(element).draggable({
+                revert: true,
+                start: function(event, ui) {
+                    // remove children of source el
+                    ReactDOM.unmountComponentAtNode($(element).next()[0]);
+                    $(element).children('button').removeClass('opened');
+                    self._updateParent($(element).children('button'));
+                }
+            });
+            $(element).droppable({
+                accept: $['id^="draggable_"'],
+                drop: function(event, ui)
+                {
+                    // remove children of destination el
+                    ReactDOM.unmountComponentAtNode($(element).next()[0]);
+                    $(element).children('button').removeClass('opened');
+                    self._updateParent($(element).children('button'));
+        
+                    // swap content and button
+                    // ...not working now. need check!
+                    var source = $(ui.draggable).parent().parent().parent();
+                    console.log(source.parent().children());
+                    $.each(source.parent().children(), function(index, value) {
+                        if (element_id === $(value).children().children().children()[1].id){
+                            var copy_to = $(element).html();
+                            var copy_from = $(ui.draggable).html();
+                            $(ui.draggable).html(copy_to);
+                            $(element).html(copy_from);
+                        }
+                    });
+                }
+            });
+        })
+        
+    }
+
+    /**
+     * resize window
+     * drag to horizon scroll
+     */
+    _updateEnv() {
+        // resize window
+        $('.wrapper').width($( window ).width()/5*4);
+        $('.wrapper').height($( window ).height()/5*4);
+
+        // drag to scroll x
+        const slider = document.querySelector('.wrapper');
+        let isDown = false;
+        let startX;
+        let scrollLeft;
+
+        slider.addEventListener('mousedown', (e) => {
+            isDown = true;
+            slider.classList.add('active');
+            startX = e.pageX - slider.offsetLeft;
+            scrollLeft = slider.scrollLeft;
+        });
+        slider.addEventListener('mouseleave', () => {
+            isDown = false;
+            slider.classList.remove('active');
+        });
+        slider.addEventListener('mouseup', () => {
+            isDown = false;
+            slider.classList.remove('active');
+        });
+        slider.addEventListener('mousemove', (e) => {
+            if(!isDown) return;
+            e.preventDefault();
+            const x = e.pageX - slider.offsetLeft;
+            const walk = (x - startX) * 3; //scroll-fast
+            slider.scrollLeft = scrollLeft - walk;
+        });
+    }
+
+    componentDidMount() {
+        this._updateEnv();
+    }
+
+    render() {
+        return ( 
+            <div className="wrapper">
+                <p>Demo tree view reactjs</p>
+                <div className="container">
+                    <div className="item-wrapper">
+                        <div className="item-node" id="draggable_root" draggable="true">
+                            <div className="item-content-container">
+                                <div className="item-content">
+                                    This is test
+                                </div>
+                            </div>
+                            <button className="btn-group-collapse" type="button" onClick={this.collapseChild.bind(this, 4)}>4 &gt;</button>
+                        </div>
+                        <div className="children-wrapper"></div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+    
+}
